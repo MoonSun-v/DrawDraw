09d44e3a91e9999618b1798f284476d02ad03793
MoonSun <98299225+MoonSun-v@users.noreply.github.com>
2024-05-12 00:38:20 +0900

    🔨 Feat | DotLine 점 충돌 구현
    - 점 이어 그리기 기본 구현 완료
    (마우스-점 충돌 감지하는 방식 사용)

---
+++ DrawDraw/Assets/Scripts/DotLineManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotLineManager : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Vector3 MousePosition;
+
+    private void Awake()
+    {
+        // "Maincamera" 태그를 가지고 있는 오브젝트 담색 후 Camera 컴포넌트 정보 전달
+        mainCamera = Camera.main;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 마우스 누르고 있는 동안 
+        if (Input.GetMouseButton(0)) 
+        {
+            // 마우스 클릭 위치 값 -> 월드 좌표 위치 값 
+            MousePosition= mainCamera.ScreenToWorldPoint(Input.mousePosition); 
+
+            RaycastHit2D hit = Physics2D.Raycast(MousePosition, transform.forward, 15f);
+            if (hit) // 충돌이 있다면 
+            {
+                // 해당 오브젝트 콜리더 비활성화 (충돌 1번만 발생시키기 위해)
+                hit.transform.GetComponent<CircleCollider2D>().enabled = false;
+                print("점과 충돌했습니다.");
+            }
+
+        }
+    }
+}
