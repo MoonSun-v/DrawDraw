a819b776eaf948982f67067e93580e5455571024
HanHyeBeen <122758695+HanHyeBeen@users.noreply.github.com>
2024-05-13 23:27:17 +0900

    🔨 Feat | 칠교 수정
    -조각이 도안보다 위로 오게 수정
    -정답 위치로만 올라가는 거 수정
    ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
    정답 판별 script 작성 예정

--- DrawDraw/Assets/Scripts/Tangram.cs
+++ DrawDraw/Assets/Scripts/Tangram.cs
@@ -6,30 +6,24 @@ public class Tangram : MonoBehaviour
 {
     public GameObject correctForm;
     private bool moving;
-    private bool finish;
 
     private float startPosX;
     private float startPosY;
 
-    private Vector2 resetPosition;
-
     void Start()
     {
-        resetPosition = this.transform.localPosition;
+
     }
 
     void Update()
     {
-        if (finish == false)
+        if (moving)
         {
-            if (moving)
-            {
-                Vector2 mousePos;
-                mousePos = Input.mousePosition;
-                mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            Vector2 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
-                this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY);
-            }
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY);
         }
     }
 
@@ -51,15 +45,5 @@ public class Tangram : MonoBehaviour
     private void OnMouseUp()
     {
         moving = false;
-        if (Mathf.Abs(this.transform.localPosition.x - correctForm.transform.localPosition.x) <= 0.5f &&
-           Mathf.Abs(this.transform.localPosition.y - correctForm.transform.localPosition.y) <= 0.5f)
-        {
-            this.transform.localPosition = new Vector2(correctForm.transform.localPosition.x, correctForm.transform.localPosition.y);
-            finish = true;
-        }
-        else
-        {
-            this.transform.localPosition = new Vector2(resetPosition.x, resetPosition.y);
-        }
     }
 }
