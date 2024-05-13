4ca2e828b30f72abfc8310e547e6b0c56eb6f049
HanHyeBeen <122758695+HanHyeBeen@users.noreply.github.com>
2024-05-13 20:02:11 +0900

    🔨 Feat | 개발 _ 칠교
    각 조각의 위치에 드랍 되게 했는데 생각해보니까 그렇게 하면 안되겠네요
    + 조각이 왜 뒤에 있는지 알다가도 모르겠는 상황

--- DrawDraw/Assets/Scripts/Tangram.cs
+++ DrawDraw/Assets/Scripts/Tangram.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class Tangram : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject correctForm;
+    private bool moving;
+    private bool finish;
+
+    private float startPosX;
+    private float startPosY;
+
+    private Vector2 resetPosition;
+
     void Start()
     {
-        
+        resetPosition = this.transform.localPosition;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (finish == false)
+        {
+            if (moving)
+            {
+                Vector2 mousePos;
+                mousePos = Input.mousePosition;
+                mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+                this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY);
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            moving = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        moving = false;
+        if (Mathf.Abs(this.transform.localPosition.x - correctForm.transform.localPosition.x) <= 0.5f &&
+           Mathf.Abs(this.transform.localPosition.y - correctForm.transform.localPosition.y) <= 0.5f)
+        {
+            this.transform.localPosition = new Vector2(correctForm.transform.localPosition.x, correctForm.transform.localPosition.y);
+            finish = true;
+        }
+        else
+        {
+            this.transform.localPosition = new Vector2(resetPosition.x, resetPosition.y);
+        }
     }
 }
