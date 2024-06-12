bb3a43b1fa8ba1ae28403e623ac9f57a085b68eb
MoonSun <98299225+MoonSun-v@users.noreply.github.com>
2024-06-12 16:55:00 +0900

    🔨 Feat | Map 저녁 시간대 설정
    - 오후6시~오전5시 : 맵 화면 이미지를 저녁이미지로 변경
    
    (현재 임시이미지, 추후 GUI완성 시 이미지 변경 필요)

---
+++ DrawDraw/Assets/Scripts/Map/MapManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public Sprite newSprite;   // 변경할 스프라이트를 참조할 변수
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        // 현재 시간 가져오기
+        DateTime currentTime = DateTime.Now;
+
+
+        // 시간 출력
+        Debug.Log("현재 시간: " + currentTime);
+
+        // 현재 오브젝트의 SpriteRenderer 컴포넌트를 가져옴
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // 시간 확인 및 스프라이트 변경
+        CheckAndChangeSprite(currentTime);
+    }
+
+    void Update()
+    {
+        // 매 프레임마다 현재 시간 업데이트 및 출력
+        // DateTime currentTime = DateTime.Now;
+        // Debug.Log("현재 시간: " + currentTime);
+    }
+
+    void CheckAndChangeSprite(DateTime currentTime)
+    {
+        // 오후 6시부터 다음날 오전 5시 사이인지 확인
+        if (currentTime.Hour >= 18 || currentTime.Hour < 5)
+        {
+            // 스프라이트를 변경
+            spriteRenderer.sprite = newSprite;
+        }
+    }
+}
