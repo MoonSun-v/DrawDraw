98b729c0f6b8a8f179349db5b161fdee577fa243
MoonSun <98299225+MoonSun-v@users.noreply.github.com>
2024-08-16 22:46:41 +0900

    🔨 Feat | Map Profile 추가
    - 맵 화면 상단의 프로필 상태바 추가
    
    ( 추후, 데이터와 연결 필요 )
    
    ( GUI 적용 완료 시, CameraScroller의 카메라 이동 범위 조절 필요 )

--- DrawDraw/Assets/Scripts/common/CameraScroller.cs
+++ DrawDraw/Assets/Scripts/common/CameraScroller.cs
@@ -2,26 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// 카메라 스크롤링 기능 구현
+
 public class CameraScroller : MonoBehaviour
 {
     public float scrollSpeed = 0.5f;
+
+    // 카메라 이동 범위 제한하는 y축 값 
     public float minY;
     public float maxY;
 
-    private Vector3 touchStart;
+    private Vector3 touchStart;  
 
     void Update()
     {
+        // 입력 감지 되었을 때 : 마우스 위치 -> 월드 좌표 변환
         if (Input.GetMouseButtonDown(0))
         {
             touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
 
+        // 클릭 상태에서 마우스 이동 시, 카메라를 이동 한다.
+        // ( ★ 최종 맵 배경 GUI 제작 완료 시 -> minY, maxY 조절 필요 )
         if (Input.GetMouseButton(0))
         {
             Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Camera.main.transform.position += new Vector3(0, direction.y * scrollSpeed, 0);
 
+            // 이동 위치 제한
             float clampedY = Mathf.Clamp(Camera.main.transform.position.y, minY, maxY);
             Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, clampedY, Camera.main.transform.position.z);
         }
