83e25c796f12b5f39ab776a6cf7c12a248946a40
MoonSun <98299225+MoonSun-v@users.noreply.github.com>
2024-08-18 01:54:47 +0900

    🔨 Feat | Profile 작성 씬 구현
    - 강아지 or 고양이 캐릭터 선택
    - 이름 입력 받기 ( 공백, 특수 문자 입력에서 제외 )
    
    둘 중 하나라도 입력 없으면 진행X, 현재는 로그 출력

---
+++ DrawDraw/Assets/Scripts/01.Prologue/ProfileManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+
+public class ProfileManager : MonoBehaviour
+{
+    public InputField NameInput;
+    private string PlayerName = null;
+
+    private bool isDog;
+    private bool isCat;
+
+    void Start()
+    {
+        // ÀÌ¸§ ÀÔ·Â¶õÀÇ ±ÛÀÚ ¼ö¸¦ 6ÀÚ·Î Á¦ÇÑ
+        NameInput.characterLimit = 6;
+
+        // Æ¯¼ö¹®ÀÚ, °ø¹é ÇÊÅÍ¸µ
+        NameInput.onValueChanged.AddListener(ValidateInput);
+    }
+
+    // °­¾ÆÁö ¼±ÅÃ
+    public void SelectDog()
+    {
+        isDog = true; isCat = false;
+    }
+
+    // °í¾çÀÌ ¼±ÅÃ
+    public void SelectCat()
+    {
+        isDog = false; isCat = true;
+    }
+
+    // ¿Ï·á ¹öÆ° Å¬¸¯
+    public void Finish()
+    {
+        // PlayerName ¾÷µ¥ÀÌÆ®
+        PlayerName = NameInput.GetComponent<InputField>().text;
+
+
+        // Ä³¸¯ÅÍ¿Í ÀÌ¸§ °Ë»ç
+        if (string.IsNullOrEmpty(PlayerName))
+        {
+            print(isDog || isCat ? "ÀÌ¸§À» Àû¾îÁÖ¼¼¿ä" : "Ä³¸¯ÅÍ¸¦ ¼±ÅÃÇÏ°í ÀÌ¸§À» Àû¾îÁÖ¼¼¿ä");
+            return;
+        }
+
+        if (!isDog && !isCat)
+        {
+            print("Ä³¸¯ÅÍ¸¦ ¼±ÅÃÇØ ÁÖ¼¼¿ä");
+            return;
+        }
+
+
+        // ÇÃ·¹ÀÌ¾î Á¤º¸ Ãâ·Â
+        print("ÇÃ·¹ÀÌ¾î ÀÌ¸§ = " + PlayerName);
+
+        if (isDog) { print("ÇÃ·¹ÀÌ¾î Ä³¸¯ÅÍ = °­¾ÆÁö"); }
+        if (isCat) { print("ÇÃ·¹ÀÌ¾î Ä³¸¯ÅÍ = °í¾çÀÌ"); }
+
+    }
+
+
+    // Æ¯¼ö¹®ÀÚ ÇÊÅÍ¸µ ¸Ş¼­µå
+    void ValidateInput(string input)
+    {
+        // ¼ıÀÚ, ¾ËÆÄºª, ÇÑ±Û Çã¿ë, Æ¯¼ö¹®ÀÚ¿Í °ø¹éÀº Á¦°Å
+        string filtered = Regex.Replace(input, @"[^a-zA-Z0-9°¡-ÆR]", "");
+        if (input != filtered)
+        {
+            NameInput.text = filtered;
+        }
+    }
+}
