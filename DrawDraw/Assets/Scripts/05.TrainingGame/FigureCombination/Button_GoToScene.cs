2d3c9fa3c689391a9c266346458b1625997ca704
joou737 <joou737@swu.ac.kr>
2024-09-16 16:16:14 +0900

    💚 Add | 도형조합 모든 씬 추가 완료
    - 1단계부터 3단계 도안 선택과 도형 맞추기 씬 추가완료
    - 기본적인 진행 가능
    
    - 각 씬 디테일 수정해야한다.

--- DrawDraw/Assets/Scripts/05.TrainingGame/FigureCombination/Button_GoToScene.cs
+++ DrawDraw/Assets/Scripts/05.TrainingGame/FigureCombination/Button_GoToScene.cs
@@ -5,6 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class Button_GoToScene : MonoBehaviour
 {
+    // 공통
+    public void ChangeScene_map()
+    {
+        SceneManager.LoadScene("MapScene");
+        Debug.Log("맵 화면으로 이동합니다.");
+    }
+
+    public void OnRestartButtonClick()
+    {
+        // 현재 활성화된 씬을 다시 로드
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log("처음부터 다시 시작합니다.");
+    }
+    // 1단계
     public void ChangeScene_Sun()
     {
         SceneManager.LoadScene("1Sun");
@@ -17,16 +31,29 @@ public class Button_GoToScene : MonoBehaviour
         Debug.Log("바람개비 도형 조합을 진행합니다.");
     }
 
-    public void ChangeScene_map()
+    //2단계
+    public void ChangeScene_Rocket()
     {
-        SceneManager.LoadScene("MapScene");
-        Debug.Log("맵 화면으로 이동합니다.");
+        SceneManager.LoadScene("2Rocket");
+        Debug.Log("로켓 도형 조합을 진행합니다.");
     }
 
-    public void OnRestartButtonClick()
+    public void ChangeScene_Ship()
     {
-        // 현재 활성화된 씬을 다시 로드
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        Debug.Log("처음부터 다시 시작합니다.");
+        SceneManager.LoadScene("2Ship");
+        Debug.Log("배 도형 조합을 진행합니다.");
+    }
+
+    //3단계
+    public void ChangeScene_Person()
+    {
+        SceneManager.LoadScene("3Person");
+        Debug.Log("사람 도형 조합을 진행합니다.");
+    }
+
+    public void ChangeScene_TheTrain()
+    {
+        SceneManager.LoadScene("3TheTrain");
+        Debug.Log("기차 도형 조합을 진행합니다.");
     }
 }
