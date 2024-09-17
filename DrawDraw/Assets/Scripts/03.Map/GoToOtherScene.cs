feb4b79b33c95bf5177a3f2220665227ec34c50e
MoonSun <98299225+MoonSun-v@users.noreply.github.com>
2024-09-17 17:32:09 +0900

    🔨 Feat | Map 훈련게임 스테이지 연결
    맵 화면에서 각 훈련 게임 스테이지로의 이동 구현
    
    - 추후 아이콘 완성 시, 도전불가능/도전가능/해결완료 나누기 필요
    - 게임 끝내고 나왔을 때 최근 플레이 진행한 위치로 이동되도록 수정 필요

--- DrawDraw/Assets/Scripts/03.Map/GoToOtherScene.cs
+++ DrawDraw/Assets/Scripts/03.Map/GoToOtherScene.cs
@@ -5,43 +5,47 @@ using UnityEngine.SceneManagement;
 
 public class GoToOtherScene : MonoBehaviour
 {
-    public void GoToMapButton()
-    {
-        SceneManager.LoadScene("MapScene");
-    }
 
-    public void GoToSelectSceneButton()
-    {
-        SceneManager.LoadScene("SelectScene");
-    }
+    public void GoToMapButton() { SceneManager.LoadScene("MapScene"); }
 
-    public void GoToDotLineButton()
-    {
-        SceneManager.LoadScene("DotLineScene");
-    }
+    public void GoToSelectSceneButton() { SceneManager.LoadScene("SelectScene"); }
 
-    public void GoToLineButton()
-    {
-        SceneManager.LoadScene("1LineScene");
-    }
 
-    public void GoToScratchButton()
-    {
-        SceneManager.LoadScene("ScratchScene");
-    }
 
-    public void GoToTangramButton()
-    {
-        SceneManager.LoadScene("TangramScene");
-    }
+    // ★ [ 각 훈련 게임 스테이지로 이동하는 버튼 메소드 ] ★
 
-    public void GoToFigureCombinationButton()
-    {
-        SceneManager.LoadScene("1FigurSelect");
-    }
 
-    public void GoToPuzzle()
-    {
-        SceneManager.LoadScene("PuzzleScene");
-    }
+    // 점선 따라 그리기
+    public void GoToDotLineButton_1() { SceneManager.LoadScene("DotLineScene1"); }
+    public void GoToDotLineButton_2() { SceneManager.LoadScene("DotLineScene2"); }
+    public void GoToDotLineButton_3() { SceneManager.LoadScene("DotLineScene3"); }
+
+
+    // 선 따라 그리기
+    public void GoToLineButton_1() { SceneManager.LoadScene("1LineScene"); }
+    public void GoToLineButton_2() { SceneManager.LoadScene("2LineScene"); }
+    public void GoToLineButton_3() { SceneManager.LoadScene("3LineScene"); }
+    public void GoToLineButton_4() { SceneManager.LoadScene("4LineScene"); }
+    public void GoToLineButton_5() { SceneManager.LoadScene("5LineScene"); }
+    public void GoToLineButton_6() { SceneManager.LoadScene("6LineScene"); }
+
+
+    // 스크래치
+    public void GoToScratchButton_1() { SceneManager.LoadScene("ScratchScene1"); }
+    public void GoToScratchButton_2() { SceneManager.LoadScene("ScratchScene2"); }
+
+
+    // 칠교 -> 씬 추가 필요
+    public void GoToTangramButton_1() { SceneManager.LoadScene("TangramScene"); }
+
+
+    // 도형 조합
+    public void GoToFigureCombinationButton_1() { SceneManager.LoadScene("1FigurSelect"); }
+    public void GoToFigureCombinationButton_2() { SceneManager.LoadScene("2FigurSelect"); }
+    public void GoToFigureCombinationButton_3() { SceneManager.LoadScene("3FigurSelect"); }
+
+
+    // 퍼즐 -> 씬 추가 필요 
+    public void GoToPuzzle_1() { SceneManager.LoadScene("PuzzleScene"); }
+
 }
