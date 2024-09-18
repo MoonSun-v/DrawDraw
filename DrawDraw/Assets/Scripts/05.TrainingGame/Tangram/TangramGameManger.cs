c2457e3a4d992144f059b21684ae5cc8a926994c
HanHyeBeen <122758695+HanHyeBeen@users.noreply.github.com>
2024-09-18 13:44:51 +0900

    🔨 Feat | Tangram Lv2 완료

--- DrawDraw/Assets/Scripts/05.TrainingGame/Tangram/TangramGameManger.cs
+++ DrawDraw/Assets/Scripts/05.TrainingGame/Tangram/TangramGameManger.cs
@@ -6,17 +6,23 @@ using UnityEngine.UI;
 public class TangramGameManager : MonoBehaviour
 {
     public GameObject patternSelectPanel;
-    public Image patternImage;
+    public GameObject patternbutton;
     public Sprite[] patternImages;
+    private Image patternButtonImage;
     public GameObject[] patternSilhouettes;
     public GameObject[] patternPieces;
     public GameObject[] button;
 
     public Text explainText;
-    
-    // Start is called before the first frame update
+
+    private Sprite initialPatternImage; // 처음 선택한 이미지를 저장할 변수
+    private bool isPatternChanged = false; // 패턴이 변경되었는지 확인하는 변수
+
+
     void Start()
     {
+        patternButtonImage = patternbutton.GetComponent<Image>();
+
         ShowPatternSelect();
 
         //explainText.text = "조각을 드래그해서 맞춰보자!";
@@ -28,7 +34,7 @@ public class TangramGameManager : MonoBehaviour
         //explainText.text = "어떤 도형을 맞춰볼래?";
 
         patternSelectPanel.SetActive(true);
-        patternImage.gameObject.SetActive(false);
+        patternButtonImage.gameObject.SetActive(true);
         foreach (var silhouette in patternSilhouettes)
         {
             silhouette.SetActive(false);
@@ -41,13 +47,12 @@ public class TangramGameManager : MonoBehaviour
         {
             btn.gameObject.SetActive(false);
         }
-
     }
 
     public void SelectPattern(int patternIndex)
     {
         patternSelectPanel.SetActive(false);
-        patternImage.gameObject.SetActive(true);
+        patternButtonImage.gameObject.SetActive(true); // 패턴 버튼 이미지 활성화
         foreach (var silhouette in patternSilhouettes)
         {
             silhouette.SetActive(false);
@@ -63,10 +68,45 @@ public class TangramGameManager : MonoBehaviour
 
         if (patternIndex >= 0 && patternIndex < patternImages.Length && patternIndex < patternSilhouettes.Length)
         {
-            patternImage.sprite = patternImages[patternIndex];
+            initialPatternImage = patternImages[patternIndex];
+
+            patternButtonImage.sprite = patternImages[patternIndex];
             patternSilhouettes[patternIndex].SetActive(true);
             patternPieces[patternIndex].SetActive(true);
             button[patternIndex].gameObject.SetActive(true);
+
+            StartCoroutine(ChangePatternImageAfterDelay(3.0f));
         }
+    }
+
+    private IEnumerator ChangePatternImageAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (patternImages.Length > 2)
+        {
+            patternButtonImage.sprite = patternImages[2];
+            isPatternChanged = true;
+        }
+    }
+
+    public void OnPatternButtonClick()
+    {
+        // 패턴이 변경된 상태라면 처음 이미지로 3초간 돌아왔다가 다시 patternImages[2]로 변경
+        if (isPatternChanged)
+        {
+            StartCoroutine(ShowInitialPatternTemporarily());
+        }
+    }
+
+    public IEnumerator ShowInitialPatternTemporarily()
+    {
+        // 처음 선택했던 패턴 이미지를 3초 동안 보여줌
+        patternButtonImage.sprite = initialPatternImage;
+
+        yield return new WaitForSeconds(3.0f);
+
+        // 다시 patternImages[2]로 돌아옴
+        patternButtonImage.sprite = patternImages[2];
     }
 }
