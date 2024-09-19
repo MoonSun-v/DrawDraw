8952b045c58d9c96b15af6e0e7cd5cac32cbfc83
HanHyeBeen <122758695+HanHyeBeen@users.noreply.github.com>
2024-09-19 19:07:26 +0900

    🔨 Feat | 보호자 메뉴 버튼 구현

--- DrawDraw/Assets/Scripts/06.Parents/ParentsManager.cs
+++ DrawDraw/Assets/Scripts/06.Parents/ParentsManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ParentsManager : MonoBehaviour
 {
@@ -8,10 +9,18 @@ public class ParentsManager : MonoBehaviour
     public GameObject SoundCanvas;
     public GameObject ExplainCanvas;
 
+    public GameObject[] MenuButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StatisticsCanvas.SetActive(true);
+        SoundCanvas.SetActive(false);
+        ExplainCanvas.SetActive(false);
+
+        SetTransparency(MenuButton[0], 1);
+        SetTransparency(MenuButton[1], 0);
+        SetTransparency(MenuButton[2], 0);
     }
 
     // Update is called once per frame
@@ -20,24 +29,53 @@ public class ParentsManager : MonoBehaviour
         
     }
 
-    public void Statistics()
+    private void SetTransparency(GameObject button, float alpha)
+    {
+        if (button != null)
+        {
+            Image buttonImage = button.GetComponent<Image>(); // Image 컴포넌트 가져오기
+            if (buttonImage != null)
+            {
+                Color color = buttonImage.color;
+                color.a = alpha; // 투명도 설정 (0 = 완전히 투명, 1 = 완전히 불투명)
+                buttonImage.color = color;
+            }
+        }
+    }
+
+    public void OnStatisticsButtonClick()
     {
         StatisticsCanvas.SetActive(true);
+        SetTransparency(MenuButton[0], 1);
+
         SoundCanvas.SetActive(false);
+        SetTransparency(MenuButton[1], 0);
+
         ExplainCanvas.SetActive(false);
+        SetTransparency(MenuButton[2], 0);
     }
 
-    public void Sound()
+    public void OnSoundButtonClick()
     {
         StatisticsCanvas.SetActive(false);
+        SetTransparency(MenuButton[0], 0);
+
         SoundCanvas.SetActive(true);
+        SetTransparency(MenuButton[1], 1);
+
         ExplainCanvas.SetActive(false);
+        SetTransparency(MenuButton[2], 0);
     }
 
-    public void Explain()
+    public void OnExplainButtonClick()
     {
         StatisticsCanvas.SetActive(false);
+        SetTransparency(MenuButton[0], 0);
+
         SoundCanvas.SetActive(false);
+        SetTransparency(MenuButton[1], 0);
+
         ExplainCanvas.SetActive(true);
+        SetTransparency(MenuButton[2], 1);
     }
 }
