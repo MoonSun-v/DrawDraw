00b372cfb9ea3d2dc69617eaa2fe10271514dc3f
joou737 <joou737@swu.ac.kr>
2024-10-01 16:16:44 +0900

    💚 Add | SOUND test 내레이션 추가
    - 클로바 더빙을 사용한 목소리 추가
    - Test_ColorClassifyScene 목소리 더빙 까먹어서 임의로 다른 오디오 추가함

---
+++ DrawDraw/Assets/Scripts/08.Etc/SequentialAudio.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequentialAudio : MonoBehaviour
+{
+    public AudioSource firstAudioSource;  // 1번 사운드
+    public AudioSource secondAudioSource; // 2번 사운드
+
+    private bool hasPlayedSecondSound = false;  // 2번 사운드가 재생되었는지 체크하는 변수
+
+    void Start()
+    {
+        // 첫 번째 사운드 재생을 시작합니다.
+        StartCoroutine(PlaySequentialSounds());
+    }
+
+    IEnumerator PlaySequentialSounds()
+    {
+        // 첫 번째 사운드를 재생합니다.
+        firstAudioSource.Play();
+
+        // 첫 번째 사운드가 끝날 때까지 대기합니다.
+        yield return new WaitForSeconds(firstAudioSource.clip.length);
+
+        // 두 번째 사운드가 아직 재생되지 않았을 때만 실행
+        if (!hasPlayedSecondSound)
+        {
+            // 두 번째 사운드를 재생합니다.
+            secondAudioSource.Play();
+            hasPlayedSecondSound = true;  // 2번 사운드가 재생되었음을 표시
+        }
+    }
+}
