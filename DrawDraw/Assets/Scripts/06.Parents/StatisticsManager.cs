2f4ced4f6ce40c83ff04313b98826066750bea09
MoonSun <98299225+MoonSun-v@users.noreply.github.com>
2024-10-02 02:00:23 +0900

    🔨 Feat | ParentPage GUI 적용 및 초기값 세팅
    - 보호자 페이지 GUI 적용
    - 항목별 통계 클릭된 버튼 표시 및 첫 통계 세팅

--- DrawDraw/Assets/Scripts/06.Parents/StatisticsManager.cs
+++ DrawDraw/Assets/Scripts/06.Parents/StatisticsManager.cs
@@ -1,20 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StatisticsManager : MonoBehaviour
 {
-    public void ResultIndexClick(int index)
+    public Text[] buttonTexts;
+
+    public ImageLoad imageLoad;
+
+    private void Start()
     {
-        switch (index)
+        OnButtonClick(0);
+        imageLoad.ResultIndexClick(1);
+    }
+
+    public void OnButtonClick(int clickedIndex)
+    {
+        // 모든 버튼 텍스트의 불투명도 변경
+        for (int i = 0; i < buttonTexts.Length; i++)
         {
-            case 1:
-                // TestResults[0]~TestResults[4]의 Game1Img 이미지들을 차례대로 5가지 Image에 띄운다.
-                break;
-            
-            default:
-                Debug.LogWarning("유효하지 않은 인덱스입니다.");
-                break;
+            if (i == clickedIndex)
+            {
+                // 클릭된 버튼의 텍스트는 불투명도 100%
+                buttonTexts[i].color = new Color(buttonTexts[i].color.r, buttonTexts[i].color.g, buttonTexts[i].color.b, 1f);
+            }
+            else
+            {
+                // 나머지 버튼의 텍스트는 불투명도 50%
+                buttonTexts[i].color = new Color(buttonTexts[i].color.r, buttonTexts[i].color.g, buttonTexts[i].color.b, 0.5f);
+            }
         }
     }
+
 }
