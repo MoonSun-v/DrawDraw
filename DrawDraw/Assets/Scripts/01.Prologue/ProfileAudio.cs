a99a5da94e846c47b2963f12f20df57290db612c
joou737 <joou737@swu.ac.kr>
2024-10-18 01:16:29 +0900

    🎨 Design | Profile 아트 추가
    - 이미지 추가
    - 캐릭터 인사말 추가
    
    ⚠️ 완성 버튼 누르면 NULL 에러로 다음 씬으로 넘어가지 않음⚠️

---
+++ DrawDraw/Assets/Scripts/01.Prologue/ProfileAudio.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfileAudio : MonoBehaviour
+{
+    public AudioSource audioSource1;  // 버튼 1의 오디오 소스
+    public AudioSource audioSource2;  // 버튼 2의 오디오 소스
+
+    // 버튼 1이 클릭되었을 때 실행될 함수
+    public void PlayAudio1()
+    {
+        // 버튼 2의 오디오가 재생 중이라면 중지
+        if (audioSource2.isPlaying)
+        {
+            audioSource2.Stop();
+        }
+
+        // 버튼 1의 오디오를 재생
+        audioSource1.Play();
+    }
+
+    // 버튼 2가 클릭되었을 때 실행될 함수
+    public void PlayAudio2()
+    {
+        // 버튼 1의 오디오가 재생 중이라면 중지
+        if (audioSource1.isPlaying)
+        {
+            audioSource1.Stop();
+        }
+
+        // 버튼 2의 오디오를 재생
+        audioSource2.Play();
+    }
+}
