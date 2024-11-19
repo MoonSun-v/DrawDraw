16a9cedac62c2b49385fe14d2c55f77023e66918
MoonSun <98299225+MoonSun-v@users.noreply.github.com>
2024-11-20 05:00:33 +0900

    🔨 Feat | StartScene에서 Test연결
    이전에 플레이 했을 때, 테스트 도중 게임을 종료 했었으면 다음에 플레이 할 때 시작화면에서 바로 테스트 시작화면으로 넘어가도록 함

--- DrawDraw/Assets/Scripts/00.Start/StartManager.cs
+++ DrawDraw/Assets/Scripts/00.Start/StartManager.cs
@@ -11,29 +11,70 @@ public class StartManager : MonoBehaviour
     // 
     public void GameStart()
     {
-
         if (GameData.instance.testdata.TestResults.ContainsKey(0))
         {
-            TestResultData resultData = GameData.instance.testdata.TestResults[0];
+            bool shouldGoToTestStartScene = false;
+            int playerExp = GameData.instance.playerdata.PlayerExp;
 
-            if (resultData.Game10Score != 0)
+            // PlayerExp에 따라 확인할 TestResults 키 결정
+            if (playerExp >= 40 && playerExp < 80)
             {
-                Debug.Log("기존 데이터가 존재합니다");
+                shouldGoToTestStartScene = CheckTestResultForGame10Score(1);
+            }
+            else if (playerExp >= 80 && playerExp < 120)
+            {
+                shouldGoToTestStartScene = CheckTestResultForGame10Score(2);
+            }
+            else if (playerExp >= 120 && playerExp < 160)
+            {
+                shouldGoToTestStartScene = CheckTestResultForGame10Score(3);
+            }
+            else if (playerExp >= 160 && playerExp < 190)
+            {
+                shouldGoToTestStartScene = CheckTestResultForGame10Score(4);
+            }
+            else if (playerExp >= 190)
+            {
+                shouldGoToTestStartScene = CheckTestResultForGame10Score(5);
+            }
+
+            if (shouldGoToTestStartScene)
+            {
+                Debug.Log("조건에 따라 Game10Score가 0인 데이터가 있습니다. TestStartScene으로 이동합니다.");
+                SceneManager.LoadScene("TestStartScene");
+                return;
+            }
+
+            if (GameData.instance.testdata.TestResults[0].Game10Score != 0)
+            {
+                Debug.Log("기존 데이터가 존재합니다. MapScene으로 이동합니다.");
                 SceneManager.LoadScene("MapScene");
             }
             else
             {
                 Debug.Log("사전 테스트 정보가 충분하지 않습니다. 프롤로그에 입장합니다.");
-                SceneManager.LoadScene("Prolog"); //프로필 -> 프롤로그 영상 씬으로 수정
+                SceneManager.LoadScene("Prolog"); // 프로필 -> 프롤로그 영상 씬으로 수정
             }
         }
         else
         {
             Debug.Log("사전 테스트 정보가 없습니다. 프롤로그에 입장합니다.");
-            SceneManager.LoadScene("Prolog"); //프로필 -> 프롤로그 영상 씬으로 수정
+            SceneManager.LoadScene("Prolog"); // 프로필 -> 프롤로그 영상 씬으로 수정
+        }
+    }
+
+    private bool CheckTestResultForGame10Score(int key)
+    {
+        // 지정된 키에 대한 TestResults 검사
+        if (GameData.instance.testdata.TestResults.ContainsKey(key)) 
+        {
+            TestResultData resultData = GameData.instance.testdata.TestResults[key];
+            return resultData.Game10Score == 0;
+        }
+        else
+        {
+            return true;
         }
 
     }
-
-    
 }
