db844424401a50618b052b8c1a7d36b306442512
joou737 <joou737@swu.ac.kr>
2024-11-21 20:48:19 +0900

    ⚠️ Fix | 프롤로그 영상을 이미지로 대체
    - mp4 영상 x
    - 프레임당 이미지로 뽑아서 애니메이션으로 만듬
    - 애니메이션 끝나면 다음 씬으로 이동

--- DrawDraw/Assets/Scripts/01.Prologue/PrologueManager.cs
+++ DrawDraw/Assets/Scripts/01.Prologue/PrologueManager.cs
@@ -8,9 +8,8 @@ using UnityEngine.Video;
 public class PrologueManager : MonoBehaviour
 {
     public Button nextSceneButton; // 버튼 오브젝트를 할당
-    public float delay = 3.0f;     // 지연 시간 (초 단위)
-
-    public VideoPlayer videoPlayer;   // VideoPlayer 컴포넌트를 할당
+    public Animator animator;      // Animator 컴포넌트를 할당
+    public string animationName;   // 끝을 감지할 애니메이션 이름
 
     private void Start()
     {
@@ -18,19 +17,28 @@ public class PrologueManager : MonoBehaviour
         nextSceneButton.gameObject.SetActive(false);
         nextSceneButton.onClick.AddListener(OnNextSceneButtonClicked);
 
+        // 코루틴 시작 (애니메이션과 지연 시간 둘 중 하나 기준으로 실행)
+        StartCoroutine(WaitForAnimationEnd());
+
         // 지연 시간 후 버튼을 활성화하는 코루틴 시작
         StartCoroutine(ShowButtonAfterDelayCoroutine());
 
-        // 비디오 재생 완료 시 호출될 이벤트 등록
-        videoPlayer.loopPointReached += OnVideoEnd;
     }
 
-    private IEnumerator ShowButtonAfterDelayCoroutine()
+    private IEnumerator WaitForAnimationEnd()
     {
-        yield return new WaitForSeconds(delay);
+        // 애니메이터의 상태 정보를 가져옴
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
 
-        // 버튼 활성화
-        nextSceneButton.gameObject.SetActive(true);
+        // 애니메이션이 아직 끝나지 않았다면 대기
+        while (stateInfo.IsName(animationName) && stateInfo.normalizedTime < 1.0f)
+        {
+            yield return null;
+            stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        }
+
+        // 애니메이션이 끝났으므로 씬 전환
+        SceneManager.LoadScene("Profile");
     }
 
     private void OnNextSceneButtonClicked()
@@ -39,9 +47,11 @@ public class PrologueManager : MonoBehaviour
         SceneManager.LoadScene("Profile");
     }
 
-    private void OnVideoEnd(VideoPlayer vp)
+    private IEnumerator ShowButtonAfterDelayCoroutine()
     {
-        // 지정된 씬으로 이동
-        SceneManager.LoadScene("Profile");
+        yield return new WaitForSeconds(3.0f);
+
+        // 버튼 활성화
+        nextSceneButton.gameObject.SetActive(true);
     }
 }
