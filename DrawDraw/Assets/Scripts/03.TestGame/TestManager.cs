1f9f37400e09ae9a2fda644014a10e38e853e0ad
HanHyeBeen <122758695+HanHyeBeen@users.noreply.github.com>
2024-11-30 04:18:58 +0900

    🔨 Feat | BGM _ test countdown

--- DrawDraw/Assets/Scripts/03.TestGame/TestManager.cs
+++ DrawDraw/Assets/Scripts/03.TestGame/TestManager.cs
@@ -20,7 +20,10 @@ public class TestManager : MonoBehaviour
     private Animator animator_character;
 
     public AudioClip DogAudio;
-    private AudioSource audioSource; // 오디오 소스 컴포넌트
+    public AudioSource audioSource; // 오디오 소스 컴포넌트
+
+    public AudioClip countAudio;
+    public AudioSource countaudioSource; // 오디오 소스 컴포넌트
 
     // 플레이어 캐릭터 정보 불러오기
     bool isDog;
@@ -37,6 +40,14 @@ public class TestManager : MonoBehaviour
         animator_curtain = Curtain.GetComponent<Animator>();
         animator_character = Character.gameObject.GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
+
+        // countaudioSource 초기화
+        if (countaudioSource == null)
+        {
+            countaudioSource = gameObject.AddComponent<AudioSource>();
+            countaudioSource.clip = countAudio;  // countAudio를 연결
+            countaudioSource.playOnAwake = false;  // 자동 재생 방지
+        }
 
         isDog = !GameData.instance.playerdata.PlayerCharacter;  // 강아지면 true, 고양이면 false
         if (isDog) { Character.sprite = DogCharacter; TextImage.sprite = DogText; audioSource.clip = DogAudio; }
@@ -63,11 +74,13 @@ public class TestManager : MonoBehaviour
     {
         yield return new WaitForSeconds(1.5f);
         animator_curtain.SetBool("isStart", true);
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(2f);
+        countaudioSource.Play();
+        yield return new WaitForSeconds(1f);
         CountDown.sprite = Two;
         yield return new WaitForSeconds(1f);
         CountDown.sprite = One;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1.2f);
 
         SceneManager.LoadScene("Test_1Line");
     }
